cd79a0ba81c9d3a41ddd4cbfb6173229563cad73
Ishant <ishantdahiya2007@gmail.com>
2026-06-18 17:39:30 +0530

    server init

---
+++ server/src/config/db.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI as string);
+
+    console.log("✅ MongoDB Connected");
+  } catch (error) {
+    console.error("❌ Database Connection Error:", error);
+    process.exit(1);
+  }
+};
